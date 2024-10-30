c493f4ae57aaf8d8e8612d19d8cc20efea5d58a8
juan-murillo <b.tapias96@gmaill.com>
2024-10-30 08:25:57 -0500

    cambio de clave de la conexion php

--- Proyecto_SFT_MNS/config/database.php
+++ Proyecto_SFT_MNS/config/database.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $base = new PDO('mysql: host= localhost; dbname=proyecto_shopping_footwear_tenis_mns', 'root', '');
+    $base = new PDO('mysql: host= localhost; dbname=proyecto_shopping_footwear_tenis_mns', 'root', '123456');
     $base -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $base -> exec("SET CHARACTER SET UTF8");
 }catch(exception $e){
